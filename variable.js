8d11af3ec6210d6d119338909a9e115927fbf8d1
learnwithhkr3 <learnwithhkr3@gmail.com>
2020-07-15 17:51:53 +0530

    add variable.js

---
+++ variable.js
@@ -0,0 +1,43 @@
+console.log("connected")
+
+// var, let , const
+
+var name = "hkr hasan"
+
+name = "talib"
+var marks = 250
+console.log(marks)
+
+const city = "delhi"
+
+// city = "chennai" error
+console.log(city)
+
+
+{
+    // let marks = 200
+    console.log(marks)
+}
+
+console.log(marks)
+
+// Rule
+// 1. Cannot start with number
+// 2. can start with letter, $, _
+// 3. case sensitive
+
+// let userName = "talib"
+
+// console.log(userName)
+
+// cases
+
+/*
+1 camel  = userName
+2 cabab = user-name
+3 sanke case = user_name
+4 pascal = UserName
+
+*/
+
+
