7028c3e626071962ffd0552f7519a310bf505d1d
learnwithhkr3 <learnwithhkr3@gmail.com>
2020-07-18 16:24:54 +0530

    add datatype.js

---
+++ datatype.js
@@ -0,0 +1,26 @@
+console.log("connected");
+
+
+// 1 Primitive
+
+// String
+let name = "talib876867";
+
+console.log(typeof name);
+
+// Number
+let marks = 253;
+
+console.log(typeof marks)
+
+// Boolean
+let isLogin = false
+console.log(typeof isLogin)
+
+// Null
+let channel = null
+console.log(typeof channel)
+
+// undefined
+let abc;
+console.log(abc)
